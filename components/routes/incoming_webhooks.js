1327d66c4de871d4b12b07f0389f96fe5781f6dc
techspiderman <adm@MacRetina13s-MacBook-Pro.local>
2018-04-13 14:24:17 -0700

    posting only to heroku

--- components/routes/incoming_webhooks.js
+++ components/routes/incoming_webhooks.js
@@ -1,4 +1,9 @@
 var debug = require('debug')('botkit:incoming_webhooks');
+var express = require('express')
+var request = require('request')
+var bodyParser = require('body-parser')
+var app = express()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 function sendMessageToSlackResponseURL(responseURL, JSONmessage){
     var postOptions = {
