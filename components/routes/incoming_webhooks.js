dd95b61b3d5dc4222a41b25e3bd67842bcc95a47
techspiderman <adm@MacRetina13s-MacBook-Pro.local>
2018-04-13 14:15:17 -0700

    posting only to heroku

--- components/routes/incoming_webhooks.js
+++ components/routes/incoming_webhooks.js
@@ -1,5 +1,21 @@
 var debug = require('debug')('botkit:incoming_webhooks');
 
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+    var postOptions = {
+        uri: responseURL,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        json: JSONmessage
+    }
+    request(postOptions, (error, response, body) => {
+        if (error){
+            // handle errors as you see fit
+        }
+    })
+}
+
 module.exports = function(webserver, controller) {
 
     debug('Configured /slack/receive url');
@@ -14,5 +30,67 @@ module.exports = function(webserver, controller) {
         controller.handleWebhookPayload(req, res);
 
     });
+
+    // webserver.post('/slack/send-me-button', function(req, res) {
+
+    //     // NOTE: we should enforce the token check here
+
+    //     // respond to Slack that the webhook has been received.
+    //     res.status(200);
+    //     console.log('test button');
+    //     // Now, pass the webhook into be processed
+    //     controller.handleWebhookPayload(req, res);
+
+    // });
+
+
+     webserver.post('/slack/send-me-button', function(req, res) {
+
+    res.status(200).end() // best practice to respond with empty 200 status code
+    var reqBody = req.body
+    var responseURL = reqBody.response_url
+    if (reqBody.token != YOUR_APP_VERIFICATION_TOKEN){
+        res.status(403).end("Access forbidden")
+    }else{
+        var message = {
+            "text": "This is your first interactive message",
+            "attachments": [
+                {
+                    "text": "Building buttons is easy right?",
+                    "fallback": "Shame... buttons aren't supported in this land",
+                    "callback_id": "button_tutorial",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "yes",
+                            "text": "yes",
+                            "type": "button",
+                            "value": "yes"
+                        },
+                        {
+                            "name": "no",
+                            "text": "no",
+                            "type": "button",
+                            "value": "no"
+                        },
+                        {
+                            "name": "maybe",
+                            "text": "maybe",
+                            "type": "button",
+                            "value": "maybe",
+                            "style": "danger"
+                        }
+                    ]
+                }
+            ]
+        }
+        sendMessageToSlackResponseURL(responseURL, message);
+          console.log('test button');
+        // Now, pass the webhook into be processed
+        controller.handleWebhookPayload(req, res);
+    }
+});
+
 
 }
