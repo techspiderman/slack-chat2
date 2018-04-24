f4747986dd48c34b8fbe1a71060eee0d1af2750e
techspiderman <adm@MacRetina13s-MacBook-Pro.local>
2018-04-24 12:52:46 -0700

    posting only to heroku

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -11,58 +11,160 @@ respond immediately with a single line response.
 
 module.exports = function(controller) {
 
+
     controller.hears(['^'], 'direct_message,direct_mention,mention', function(bot, message) {
         //bot.reply(message, " " + message.team);
        
        // bot.reply(message,"Congratulations on your success! Your success:" + message.text + " has been added to your success log.");
     
-bot.createConversationInThread(message, function(err, convo) {
+bot.reply(message, {
+        attachments:[
+            {
+                title: 'Congratulations on your success! Which of your traits helped you in achieving this success?',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"knowledge",
+                        "text": "Knowledge",
+                        "value": "Knowledge",
+                        "type": "button",
+                    },
+                    {
+                        "name":"speed",
+                        "text": "Speed",
+                        "value": "Speed",
+                        "type": "button",
+                    },
+                    {
+                        "name":"focus",
+                        "text": "focus",
+                        "value": "focus",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
+    });
 
-    // create a path for when a user says YES
-    convo.addMessage({
-            text: 'How nice you gained motivation from this.',
-    },'yes_thread');
 
-    // create a path for when a user says NO
-    convo.addMessage({
-        text: 'You said no, that is too bad.',
-    },'no_thread');
 
-    // create a path where neither option was matched
-    // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
-    convo.addMessage({
-        text: 'Sorry I did not understand.',
-        action: 'default',
-    },'bad_response');
+// bot.createConversationInThread(message, function(err, convo) {
 
-    // Create a yes/no question in the default thread...
-    convo.addQuestion('Congratulations on your success! What did you gain from this success?', [
-        {
-            pattern: 'motivation',
-            callback: function(response, convo) {
-                convo.gotoThread('yes_thread');
-            },
-        },
-        {
-            pattern: 'no',
-            callback: function(response, convo) {
-                convo.gotoThread('no_thread');
-            },
-        },
-        {
-            default: true,
-            callback: function(response, convo) {
-                convo.gotoThread('bad_response');
-            },
-        }
-    ],{},'default');
+//     // create a path for when a user says YES
+//     convo.addMessage({
+//             text: 'How nice you gained motivation from this.',
+//     },'yes_thread');
 
-    convo.activate();
-});
+//     // create a path for when a user says NO
+//     convo.addMessage({
+//         text: 'You said no, that is too bad.',
+//     },'no_thread');
+
+//     // create a path where neither option was matched
+//     // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
+//     convo.addMessage({
+//         text: 'Sorry I did not understand.',
+//         action: 'default',
+//     },'bad_response');
+
+//     // Create a yes/no question in the default thread...
+//     convo.addQuestion('Congratulations on your success! What did you gain from this success?', [
+//         {
+//             pattern: 'motivation',
+//             callback: function(response, convo) {
+//                 convo.gotoThread('yes_thread');
+//             },
+//         },
+//         {
+//             pattern: 'no',
+//             callback: function(response, convo) {
+//                 convo.gotoThread('no_thread');
+//             },
+//         },
+//         {
+//             default: true,
+//             callback: function(response, convo) {
+//                 convo.gotoThread('bad_response');
+//             },
+//         }
+//     ],{},'default');
+
+//     convo.activate();
+// });
 
 
     });
 
+controller.on('send-me-button',function(bot,message) {
+
+    // reply to slash command
+   
+    bot.reply(message, {
+        attachments:[
+            {
+                title: 'Do you want to interact with my buttons?',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                        "name":"no",
+                        "text": "No",
+                        "value": "no",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
+    });
+
+})
+
+
+// receive an interactive message, and reply with a message that will replace the original
+controller.on('interactive_message_callback', function(bot, message) {
+
+    // check message.actions and message.callback_id to see what action to take...
+
+    bot.replyInteractive(message, {
+        text: '...',
+        attachments: [
+            {
+                title: 'My buttons',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes!",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                       "text": "No!",
+                        "name": "no",
+                        "value": "delete",
+                        "style": "danger",
+                        "type": "button",
+                        "confirm": {
+                          "title": "Are you sure?",
+                          "text": "This will do something!",
+                          "ok_text": "Yes",
+                          "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }
+        ]
+    });
+
+});
 
 
 };
